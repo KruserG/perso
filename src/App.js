feee95feb46fdd09a24f9985b31c0eebf079ea95
kruserg <yokariig@gmail.com>
2021-05-17 09:34:04 -0400

    new version

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons'
 //brands svg icons
 import { faLinkedin, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
-
+//react-scroll
+import { Link, Element } from 'react-scroll'
+//react
+import {Card, Button} from 'react-bootstrap'
 
 function App() {
 
@@ -35,24 +38,37 @@ function App() {
       
       </div>
 
-      <div className="d-flex justify-content-center m-5">
-        <a className="btn text-dark" target="_blank" href="https://boulbaze.com" rel="noreferrer">
-          <h1><FontAwesomeIcon opacity="0.55" size="2x" icon={faAngleDoubleDown} /></h1>
-        </a>
+      <div className="d-flex justify-content-center">
+        <Link
+        activeClass="active" 
+        to="projects" 
+        smooth={true}
+        duration={1000}
+        >
+          <span className="scroll_down"><FontAwesomeIcon icon={faAngleDoubleDown} /></span>
+        </Link>
       </div>
 
     </div>
 
      <div className="wave">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -0.5 1440 320">
          <path className="wave1" fillOpacity="0.6" d="M0,160L48,154.7C96,149,192,139,288,160C384,181,480,235,576,218.7C672,203,768,117,864,112C960,107,1056,181,1152,181.3C1248,181,1344,107,1392,69.3L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
-         <path className="wave2" fillOpacity="0.3"></path>
-         <path className="wave3" fillOpacity="0.1"></path>
+         <path className="wave2" fillOpacity="0.3" d="M0,160L48,154.7C96,149,192,139,288,160C384,181,480,235,576,218.7C672,203,768,117,864,112C960,107,1056,181,1152,181.3C1248,181,1344,107,1392,69.3L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+         <path className="wave3" fillOpacity="0.1" d="M0,160L48,154.7C96,149,192,139,288,160C384,181,480,235,576,218.7C672,203,768,117,864,112C960,107,1056,181,1152,181.3C1248,181,1344,107,1392,69.3L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
       </svg>
     </div>
+    
+    <Element name="projects" className="projects">
+      
 
+    
+    </Element>
+    
+
+
+ 
   </div>
-  
   );
 
 }
