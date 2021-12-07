5494b4ddeca1c2e8b46eed2d975891345846b716
KruserG <yokariig@gmail.com>
2021-12-07 15:44:02 -0500

    updating the ui

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 //font-awesome importing
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 //solid svg icons
-import { faAngleDoubleDown, faEye, faEyeSlash, faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDoubleDown, faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
 //brands svg icons
 import { faLinkedin, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
 //react-scroll
@@ -17,7 +17,11 @@ import {useEffect, useState} from 'react';
 //languages
 import english from './translations/english.json';
 import french from './translations/french.json';
-//logos
+//projects logos
+import easynpsLogo from './logos/easynpsLogo.jpg'
+import sunsetradioLogo from './logos/sunsetradioLogo.jpg'
+import tcornerLogo from './logos/tcornerLogo.jpg'
+//technologies logos
 import nodejsLogo from './logos/nodejs.png';
 import reactLogo from './logos/react.png';
 import phpLogo from './logos/php.png';
@@ -27,6 +31,9 @@ import bootstrapLogo from './logos/bootstrap.png';
 import photoshopLogo from './logos/photoshop.png';
 import illustratorLogo from './logos/illustrator.png';
 import mongoLogo from './logos/mongodb.png';
+//image
+import circuit from './logos/circuit.png'
+
 
 function App() {
 
@@ -59,10 +66,6 @@ if(!localStorage.getItem("language")){
 }
 
 const [selectedLanguage, setSelectedLanguage] = useState(()=> localStorage.getItem("language") === "french" ? french : english );
-
-const [toolsVisibility, setToolsVisibility] = useState("visible");
-const [eyeIcon, switchEyeIcon] = useState(faEyeSlash);
-
 const [theme, setTheme] = useState(localStorage.getItem("theme"));
 const [themeIcon, setThemeIcon] = useState(()=> theme === "light" ? faMoon : faSun);
 
@@ -74,11 +77,6 @@ const switchLanguage = ()=>{
     setSelectedLanguage(french)
     localStorage.setItem("language","french")
   }
-}
-
-const switchToolsVisibility = ()=>{
-  toolsVisibility === "visible"  ? setToolsVisibility("hidden") : setToolsVisibility("visible")
-  toolsVisibility === "visible"  ? switchEyeIcon(faEye) : switchEyeIcon(faEyeSlash) 
 }
 
 const switchTheme = ()=>{
@@ -113,51 +111,35 @@ useEffect(()=>{
 
   return (
 
-  <div>
+  <>
 
-    <div className="theme-switch">
-      <button className="button" onClick={switchTheme}><FontAwesomeIcon icon={themeIcon} /></button>
-    </div>
+  <div className="header-container">
+      <button className="theme-switch" onClick={switchTheme}><FontAwesomeIcon icon={themeIcon} /></button>
+      <button className="language-switch" onClick={switchLanguage} >{selectedLanguage.changeButtonLanguage}</button>
+  </div>
+ 
     
 
-    <div className="language-switch" onClick={switchLanguage}>
-      <button className="button">{selectedLanguage.changeButtonLanguage}</button>
-    </div>
-
     <Div100vh className="main-page">
-   
-      <div className="d-flex justify-content-center">
 
-        <div className="aboutme">
-            <h1 className="bigHello">{selectedLanguage.mainPage.welcome}</h1>
-            <h1 className="myNameIs">{selectedLanguage.mainPage.introduction}</h1>
-            <br/>
-            <h6 className="whoAmI">{selectedLanguage.mainPage.description}</h6>
-        </div>
-
+      <div className="aboutMe-container">
+          <h1 style={{fontSize:"5rem"}}>{selectedLanguage.mainPage.welcome}</h1>
+          <h4 style={{fontSize:"1.5rem"}}>{selectedLanguage.mainPage.introduction}</h4>
+          <h6 style={{fontSize:"1rem"}}>{selectedLanguage.mainPage.description}</h6>
       </div>
 
-      <div className="d-flex justify-content-center social-links">
-        
-          <a className="btn text-dark" href="https://www.linkedin.com/in/omarboulbaze/" target="_blank" rel="noreferrer"><h1><FontAwesomeIcon icon={faLinkedin} /></h1></a>
-          <a className="btn text-dark" href="https://github.com/KruserG" target="_blank" rel="noreferrer"><h1><FontAwesomeIcon icon={faGithub} /></h1></a>
-          <a className="btn text-dark" href="https://twitter.com/omarboulbaze" target="_blank" rel="noreferrer"><h1><FontAwesomeIcon icon={faTwitter} /></h1></a> 
+      <div className="social-container social-links">
+          <a className="btn text-dark" href="https://www.linkedin.com/in/omarboulbaze/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
+          <a className="btn text-dark" href="https://github.com/KruserG" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
+          <a className="btn text-dark" href="https://twitter.com/omarboulbaze" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faTwitter} /></a> 
+      </div>
+
+      <Link className="scroll_down" activeClass="active" to="experiences">
+        <span><FontAwesomeIcon icon={faAngleDoubleDown} /></span>
+      </Link>
       
-      </div>
-
-      <div className="d-flex justify-content-center">
-        <Link className="scroll_down" activeClass="active" to="experiences" smooth={true} duration={1000}>
-          <span><FontAwesomeIcon icon={faAngleDoubleDown} /></span>
-        </Link>
-      </div>
-      <div className="wave">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -0.5 1440 320">
-         <path className="wave1" fillOpacity="1" d="M0,160L48,154.7C96,149,192,139,288,160C384,181,480,235,576,218.7C672,203,768,117,864,112C960,107,1056,181,1152,181.3C1248,181,1344,107,1392,69.3L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
-         <path className="wave2" fillOpacity="0.6" d="M0,160L48,154.7C96,149,192,139,288,160C384,181,480,235,576,218.7C672,203,768,117,864,112C960,107,1056,181,1152,181.3C1248,181,1344,107,1392,69.3L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
-         <path className="wave3" fillOpacity="0.3" d="M0,160L48,154.7C96,149,192,139,288,160C384,181,480,235,576,218.7C672,203,768,117,864,112C960,107,1056,181,1152,181.3C1248,181,1344,107,1392,69.3L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
-      </svg>
-    </div>
-
+      <img className="circuit-image" src={circuit} alt=""/>
+      
     </Div100vh>
 
     
@@ -165,72 +147,70 @@ useEffect(()=>{
       <div  className="experiences">
       <div className="head">
         <h1 className="title">{selectedLanguage.experiences.title}</h1>
-        <button className="btn_dev" onClick={switchToolsVisibility}><FontAwesomeIcon icon={eyeIcon} /></button>
       </div>
 
       <div className="body">
         <a className="link" href="https://github.com/KruserG/easynps" target="_blank" rel="noreferrer">
             <div className="item">
-            <img className="logo" src="https://www.easynps.ca/static/media/easyNpsLogo.7c7e088e.png" alt="easynps-logo"/>
-            <h2 className="title">{selectedLanguage.experiences.project2.title}</h2>
-            <div className="description">
-            <p>{selectedLanguage.experiences.project2.description}</p>
-            </div>
-            <div className="tools" style={{visibility : toolsVisibility}}>
-            
-            {
-              projectSkills3.map((skill)=>{
+              <img className="logo" src={easynpsLogo} alt="easynps-logo"/>
+              <div className="project_description">
+                <h2>{selectedLanguage.experiences.project2.title}</h2>
+                <p>{selectedLanguage.experiences.project2.description}</p>
+              </div>
+              <div className="tools">
+              {   projectSkills3.map((skill)=>{ 
                 return(
-                <OverlayTrigger key={skill.name} placement="bottom" overlay={<Tooltip id={`tooltip-bottom`}>{skill.name}</Tooltip>}>
-                <img className="tool-logo"  src={skill.image} alt={skill.name}/>
-                </OverlayTrigger>
-                );
-              })
-            }           
-            </div>
+                  <OverlayTrigger key={skill.name} placement="bottom" overlay={<Tooltip id={`tooltip-bottom`}>{skill.name}</Tooltip>}>
+                  <img className="tool-logo"  src={skill.image} alt={skill.name}/>
+                  </OverlayTrigger>
+                  );
+                })
+              }           
+              </div>
           </div>
           </a>
           <a className="link" href="https://github.com/KruserG/sunsetradio-website" target="_blank" rel="noreferrer">
             <div className="item">
-            <img className="logo" src="https://imgur.com/wBUzy2f.jpg" alt="sunsetradio-logo"/>
-            <h2 className="title">{selectedLanguage.experiences.project0.title}</h2>
-            <div className="description">
-            <p>{selectedLanguage.experiences.project0.description}</p>
-            </div>
-            <div className="tools" style={{visibility : toolsVisibility}}>
-            
-            {
-              projectSkills1.map((skill)=>{
+              <img className="logo" src={sunsetradioLogo} alt="sunsetradio-logo"/>
+              <div className="project_description">
+                <h2>{selectedLanguage.experiences.project0.title}</h2>
+                <p>{selectedLanguage.experiences.project0.description}</p>
+              </div>
+              <div className="tools">
+              {   projectSkills1.map((skill)=>{ 
                 return(
-                <OverlayTrigger key={skill.name} placement="bottom" overlay={<Tooltip id={`tooltip-bottom`}>{skill.name}</Tooltip>}>
-                <img className="tool-logo"  src={skill.image} alt={skill.name}/>
-                </OverlayTrigger>
-                );
-              })
-            }           
-            </div>
+                  <OverlayTrigger key={skill.name} placement="bottom" overlay={<Tooltip id={`tooltip-bottom`}>{skill.name}</Tooltip>}>
+                  <img className="tool-logo"  src={skill.image} alt={skill.name}/>
+                  </OverlayTrigger>
+                  );
+                })
+              }           
+              </div>
           </div>
           </a>
           <a className="link" href="https://github.com/KruserG/tcorner-website" target="_blank" rel="noreferrer">
-          <div className="item">
-            <img className="logo" src="https://st3.depositphotos.com/3096625/18861/v/600/depositphotos_188612498-stock-illustration-monogram-tc-logo-hipster-initials.jpg" alt="sunsetradio-logo"/>
-            <h2 className="title">{selectedLanguage.experiences.project1.title}</h2>
-            <div className="description">
-            <p>{selectedLanguage.experiences.project1.description}</p>
-            </div>
-            <div className="tools" style={{visibility : toolsVisibility}}>
-            {
-              projectSkills2.map((skill)=>{
+            <div className="item">
+              <img className="logo" src={tcornerLogo} alt="tcorner-logo"/>
+              <div className="project_description">
+                <h2>{selectedLanguage.experiences.project1.title}</h2>
+                <p>{selectedLanguage.experiences.project1.description}</p>
+              </div>
+              <div className="tools">
+              {   projectSkills2.map((skill)=>{ 
                 return(
-                <OverlayTrigger key={skill.name} placement="bottom" overlay={<Tooltip id={`tooltip-bottom`}>{skill.name}</Tooltip>}>
-                <img className="tool-logo" src={skill.image} alt={skill.name}/>
-                </OverlayTrigger>
-                );
-              })
-            }  
-            </div>
+                  <OverlayTrigger key={skill.name} placement="bottom" overlay={<Tooltip id={`tooltip-bottom`}>{skill.name}</Tooltip>}>
+                  <img className="tool-logo"  src={skill.image} alt={skill.name}/>
+                  </OverlayTrigger>
+                  );
+                })
+              }           
+              </div>
           </div>
           </a>
+          
+          
+         
+         
       </div>
 
       <div className="quote">
@@ -246,7 +226,7 @@ useEffect(()=>{
     </Element> 
     
  
-  </div>
+  </>
 
   );
 
